584d956cca804e86dcea983f621978e512806519
mai <125476096+mai3331@users.noreply.github.com>
2024-10-22 12:16:12 +0300

    jkj

--- src/Pages/movieDetails.jsx
+++ src/Pages/movieDetails.jsx
@@ -46,7 +46,7 @@ const MovieDetails = () => {
     }
 
     const response = await fetch(
-      `http://localhost:5000/api/movies/${id}/reviews`,
+      `https://depi-back-production.up.railway.app/api/movies/${id}/reviews`,
       {
         method: "POST",
         headers: {
